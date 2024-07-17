67761b1819d17d9612912410b5db5b00c01a310a
AleenaSehar <99498362+AleenaSehar@users.noreply.github.com>
2024-07-17 20:05:50 +0000

    file has changed

---
+++ basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; /* code will be treated as the code of new version of javascript*/
+// alert(3+3) we are using nodejs, not the browser
+
+ //number => 2 to power 53
+ //bigggggggggggggggggggg   int
+ //string => ""
+ //boolean true False
+ //null ==> stanalone value
+ //undefined ==> value is not defined
+ //symbol ==> uniqueness
+
+
+ //object 
+
+ console.log(typeof null)
---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 14435
+let accountEmail = "aleenasehar319@gmail.com"
+var accountPassword = "12345"
+accountCity = "Namal"
+/* prefer not to use Var because it does not have block scope*/
+console.table([accountId, accountEmail, accountPassword, accountCity]);
